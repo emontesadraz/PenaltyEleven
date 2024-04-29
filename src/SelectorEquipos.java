011e14f04f7358f4c046b8336021e0415e70364d
estebanmontes <emontesadraz@danielcastelao.org>
2024-04-29 17:59:24 +0200

    Botones con colores

--- src/SelectorEquipos.java
+++ src/SelectorEquipos.java
@@ -27,30 +27,44 @@ public class SelectorEquipos extends JFrame{
         this.add(panel);
 
         seleccionarEqu1 = new JButton("Seleccionar");
+        seleccionarEqu1.setFont(fuenteBoton);
+        seleccionarEqu1.setBackground(colorBoton);
         seleccionarEqu1.setBounds(50, 50, 200, 50);
         panel.add(seleccionarEqu1);
 
         seleccionarEqu2 = new JButton("Seleccionar");
+        seleccionarEqu2.setFont(fuenteBoton);
+        seleccionarEqu2.setBackground(colorBoton);
         seleccionarEqu2.setBounds(550, 50, 200, 50);
         panel.add(seleccionarEqu2);
 
         jugar = new JButton("Jugar");
+        jugar.setFont(fuenteBoton);
+        jugar.setBackground(colorBoton);
         jugar.setBounds(300, 500, 200, 50);
         panel.add(jugar);
 
         flechaIzquierda = new JButton("<");
+        flechaIzquierda.setFont(fuenteBoton);
+        flechaIzquierda.setBackground(colorBoton);
         flechaIzquierda.setBounds(600, 350, 50, 50);
         panel.add(flechaIzquierda);
 
         flechaDerecha = new JButton(">");
+        flechaDerecha.setFont(fuenteBoton);
+        flechaDerecha.setBackground(colorBoton);
         flechaDerecha.setBounds(650, 350, 50, 50);
         panel.add(flechaDerecha);
 
         flechaIzquierda2 = new JButton("<");
+        flechaIzquierda2.setFont(fuenteBoton);
+        flechaIzquierda2.setBackground(colorBoton);
         flechaIzquierda2.setBounds(100, 350, 50, 50);
         panel.add(flechaIzquierda2);
 
         flechaDerecha2 = new JButton(">");
+        flechaDerecha2.setFont(fuenteBoton);
+        flechaDerecha2.setBackground(colorBoton);
         flechaDerecha2.setBounds(150, 350, 50, 50);
         panel.add(flechaDerecha2);
 
