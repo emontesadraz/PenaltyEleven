66819902255661b56384b674056e0a80dce43102
estebanmontes <emontesadraz@danielcastelao.org>
2024-04-29 17:54:23 +0200

    Botones centrados

--- src/SelectorEquipos.java
+++ src/SelectorEquipos.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class SelectorEquipos extends JFrame{
     JButton seleccionarEqu1;
@@ -7,6 +8,8 @@ public class SelectorEquipos extends JFrame{
     JButton flechaIzquierda;
     JButton flechaDerecha;
     JPanel panel;
+    JButton flechaIzquierda2;
+    JButton flechaDerecha2;
 
     public SelectorEquipos(){
         super("Penalty Eleven");
@@ -16,15 +19,18 @@ public class SelectorEquipos extends JFrame{
         this.setLocationRelativeTo(null);
         this.setResizable(false);
 
+        Font fuenteBoton = new Font("Action Man", Font.BOLD, 15);
+        Color colorBoton = new Color(214, 202, 42);
+
         panel = new JPanel();
         panel.setLayout(null);
         this.add(panel);
 
-        seleccionarEqu1 = new JButton("Seleccionar equipo 1");
+        seleccionarEqu1 = new JButton("Seleccionar");
         seleccionarEqu1.setBounds(50, 50, 200, 50);
         panel.add(seleccionarEqu1);
 
-        seleccionarEqu2 = new JButton("Seleccionar equipo 2");
+        seleccionarEqu2 = new JButton("Seleccionar");
         seleccionarEqu2.setBounds(550, 50, 200, 50);
         panel.add(seleccionarEqu2);
 
@@ -33,12 +39,21 @@ public class SelectorEquipos extends JFrame{
         panel.add(jugar);
 
         flechaIzquierda = new JButton("<");
-        flechaIzquierda.setBounds(300, 250, 50, 50);
+        flechaIzquierda.setBounds(600, 350, 50, 50);
         panel.add(flechaIzquierda);
 
         flechaDerecha = new JButton(">");
-        flechaDerecha.setBounds(450, 250, 50, 50);
+        flechaDerecha.setBounds(650, 350, 50, 50);
         panel.add(flechaDerecha);
+
+        flechaIzquierda2 = new JButton("<");
+        flechaIzquierda2.setBounds(100, 350, 50, 50);
+        panel.add(flechaIzquierda2);
+
+        flechaDerecha2 = new JButton(">");
+        flechaDerecha2.setBounds(150, 350, 50, 50);
+        panel.add(flechaDerecha2);
+
     }
 
 }
