37642af2dc0a52712c125e0fc0960e767457cd22
estebanmontes <emontesadraz@danielcastelao.org>
2024-04-30 20:01:15 +0200

    Clase Equipos y OperacionesEquipos para cargar los equipos que estarán en el juego e intento de poner los escudos

--- src/SelectorEquipos.java
+++ src/SelectorEquipos.java
@@ -8,6 +8,8 @@ import java.io.File;
 import java.io.IOException;
 
 public class SelectorEquipos extends JFrame{
+    OperacionesEquipos oe = new OperacionesEquipos();
+    JLabel[] labelEscudos = new JLabel[13];
     JButton seleccionarEqu1;
     JButton seleccionarEqu2;
     JButton jugar;
@@ -18,6 +20,10 @@ public class SelectorEquipos extends JFrame{
     JButton flechaIzquierda2;
     JButton flechaDerecha2;
     BufferedImage imagen;
+    JLabel labelEquipo1;
+    JLabel labelEquipo2;
+    int indiceEquipo1 = 0;
+    int indiceEquipo2 = 0;
 
     public SelectorEquipos(){
         super("Penalty Eleven");
@@ -37,12 +43,37 @@ public class SelectorEquipos extends JFrame{
         panel.setLayout(null);
         this.add(panel);
 
+        for (int i = 0; i < labelEscudos.length; i++) {
+            try {
+                BufferedImage escudo = ImageIO.read(new File("src/Imagenes/EscudoRaimon" + (i + 1) + ".png")); // Reemplaza con la ruta a las imágenes de los escudos de los equipos
+                labelEscudos[i] = new JLabel(new ImageIcon(escudo));
+                labelEscudos[i].setBounds(50, 140, escudo.getWidth(), escudo.getHeight()); // Ajusta las coordenadas y el tamaño según sea necesario
+                panel.add(labelEscudos[i]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         // Cargar imagen de fondo
         try {
             imagen = ImageIO.read(new File("src/Imagenes/FondoSelectorEquipos.jpeg")); // Ruta de la imagen de fondo
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        // JLabel para el equipo 1
+        labelEquipo1 = new JLabel(oe.getEquipos().get(indiceEquipo1).getNombreEquipo());
+        labelEquipo1.setFont(new Font("Action Man", Font.BOLD, 20));
+        labelEquipo1.setBounds(50, 110, 200, 30);
+        labelEquipo1.setHorizontalAlignment(JLabel.CENTER);
+        panel.add(labelEquipo1);
+
+        // JLabel para el equipo 2
+        labelEquipo2 = new JLabel(oe.getEquipos().get(indiceEquipo2).getNombreEquipo());
+        labelEquipo2.setFont(new Font("Action Man", Font.BOLD, 20));
+        labelEquipo2.setHorizontalAlignment(JLabel.CENTER);
+        labelEquipo2.setBounds(550, 110, 200, 30);
+        panel.add(labelEquipo2);
 
         // Botón seleccionar para el equipo 1
         seleccionarEqu1 = new JButton("Seleccionar");
@@ -88,6 +119,17 @@ public class SelectorEquipos extends JFrame{
         flechaIzquierda.setBackground(colorBoton);
         flechaIzquierda.setForeground(colorTexto);
         flechaIzquierda.setBounds(600, 350, 50, 50);
+        flechaIzquierda.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (indiceEquipo2 > 0) {
+                    labelEscudos[indiceEquipo2].setVisible(false); // Oculta la imagen del equipo anterior
+                    indiceEquipo2--;
+                    labelEquipo2.setText(oe.getEquipos().get(indiceEquipo2).getNombreEquipo());
+                    labelEscudos[indiceEquipo2].setVisible(true); // Muestra la imagen del equipo seleccionado
+                }
+            }
+        });
         panel.add(flechaIzquierda);
 
         // Botón flecha derecha
@@ -96,6 +138,17 @@ public class SelectorEquipos extends JFrame{
         flechaDerecha.setBackground(colorBoton);
         flechaDerecha.setForeground(colorTexto);
         flechaDerecha.setBounds(650, 350, 50, 50);
+        flechaDerecha.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (indiceEquipo2 < oe.getEquipos().size() - 1) {
+                    labelEscudos[indiceEquipo2].setVisible(false); // Oculta la imagen del equipo anterior
+                    indiceEquipo2++;
+                    labelEquipo2.setText(oe.getEquipos().get(indiceEquipo2).getNombreEquipo());
+                    labelEscudos[indiceEquipo2].setVisible(true); // Muestra la imagen del equipo seleccionado
+                }
+            }
+        });
         panel.add(flechaDerecha);
 
         // Botón flecha izquierda 2
@@ -104,6 +157,17 @@ public class SelectorEquipos extends JFrame{
         flechaIzquierda2.setBackground(colorBoton);
         flechaIzquierda2.setForeground(colorTexto);
         flechaIzquierda2.setBounds(100, 350, 50, 50);
+        flechaIzquierda2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (indiceEquipo1 > 0) {
+                    labelEscudos[indiceEquipo1].setVisible(false); // Oculta la imagen del equipo anterior
+                    indiceEquipo1--;
+                    labelEquipo1.setText(oe.getEquipos().get(indiceEquipo1).getNombreEquipo());
+                    labelEscudos[indiceEquipo1].setVisible(true); // Muestra la imagen del equipo seleccionado
+                }
+            }
+        });
         panel.add(flechaIzquierda2);
 
         // Botón flecha derecha 2
@@ -112,6 +176,17 @@ public class SelectorEquipos extends JFrame{
         flechaDerecha2.setBackground(colorBoton);
         flechaDerecha2.setForeground(colorTexto);
         flechaDerecha2.setBounds(150, 350, 50, 50);
+        flechaDerecha2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (indiceEquipo1 < oe.getEquipos().size() - 1) {
+                    labelEscudos[indiceEquipo1].setVisible(false); // Oculta la imagen del equipo anterior
+                    indiceEquipo1++;
+                    labelEquipo1.setText(oe.getEquipos().get(indiceEquipo1).getNombreEquipo());
+                    labelEscudos[indiceEquipo1].setVisible(true); // Muestra la imagen del equipo seleccionado
+                }
+            }
+        });
         panel.add(flechaDerecha2);
 
     }
