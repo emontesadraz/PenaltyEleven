615f7d15e93c1a1fe06d933b1f44e3c5e2611633
estebanmontes <emontesadraz@danielcastelao.org>
2024-05-10 13:52:29 +0200

    Paneles divididos

--- src/JuegoMultiplayer.java
+++ src/JuegoMultiplayer.java
@@ -26,7 +26,7 @@ public class JuegoMultiplayer extends JFrame {
 
     public JuegoMultiplayer() {
         // Configuramos el JFrame
-        setSize(1280, 720);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
         setTitle("Penalty Eleven");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
@@ -36,10 +36,12 @@ public class JuegoMultiplayer extends JFrame {
         setIconImage(new ImageIcon("src/Imagenes/Logo.png").getImage());
 
         // Creamos un panel
-        JPanel panel = new JPanel();
-        panel.setOpaque(false);
-        panel.setLayout(null);
-        add(panel);
+        JPanel panel1 = new JPanel();
+        panel1.setSize(1280, 720);
+        panel1.setBackground(new Color(0, 0, 0, 255));
+        panel1.setOpaque(false);
+        panel1.setLayout(null);
+        add(panel1);
 
         // Creamos los jugadores
         jugador1 = new Jugador1();
@@ -57,28 +59,28 @@ public class JuegoMultiplayer extends JFrame {
         abajoCentro = new JButton();
 
         // Configuramos la posicion de los botones
-        arribaIzquierda.setBounds(100, 100, 100, 100);
-        arribaDerecha.setBounds(1080, 100, 100, 100);
-        arribaCentro.setBounds(600, 100, 100, 100);
-        centroIzquierda.setBounds(100, 300, 100, 100);
-        centroDerecha.setBounds(1080, 300, 100, 100);
-        centroCentro.setBounds(600, 300, 100, 100);
-        abajoIzquierda.setBounds(100, 500, 100, 100);
-        abajoDerecha.setBounds(1080, 500, 100, 100);
-        abajoCentro.setBounds(600, 500, 100, 100);
+        arribaIzquierda.setBounds(150, 10, 200, 200);
+        arribaDerecha.setBounds(920, 10, 200, 200);
+        arribaCentro.setBounds(520,10, 200, 200);
+        centroIzquierda.setBounds(150, 210, 200, 200);
+        centroDerecha.setBounds(920, 210, 200, 200);
+        centroCentro.setBounds(520, 210, 200, 200);
+        abajoIzquierda.setBounds(150, 410, 200, 200);
+        abajoDerecha.setBounds(920, 410, 200, 200);
+        abajoCentro.setBounds(520, 410, 200, 200);
 
 
 
         // Añadimos los checkbox al panel
-        panel.add(arribaIzquierda);
-        panel.add(arribaDerecha);
-        panel.add(arribaCentro);
-        panel.add(centroIzquierda);
-        panel.add(centroDerecha);
-        panel.add(centroCentro);
-        panel.add(abajoIzquierda);
-        panel.add(abajoDerecha);
-        panel.add(abajoCentro);
+        panel1.add(arribaIzquierda);
+        panel1.add(arribaDerecha);
+        panel1.add(arribaCentro);
+        panel1.add(centroIzquierda);
+        panel1.add(centroDerecha);
+        panel1.add(centroCentro);
+        panel1.add(abajoIzquierda);
+        panel1.add(abajoDerecha);
+        panel1.add(abajoCentro);
 
 
     }
