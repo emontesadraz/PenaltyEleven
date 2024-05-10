9fe2363a4e97a7f9cf2d48f3c6ff26b4bd78c220
Esteban <emontesadraz@danielcastelao.org>
2024-05-10 12:34:50 +0200

    Cambiamos los checkbox por los botones

--- src/JuegoMultiplayer.java
+++ src/JuegoMultiplayer.java
@@ -11,22 +11,22 @@ public class JuegoMultiplayer extends JFrame {
     private Jugador2 jugador2;
     public static final Color colorBaseBotones = new Color(25, 25, 25);
     private final MusicManager musicManager = new MusicManager();
-    private JCheckBox arribaIzquierda;
-    private JCheckBox centroIzquierda;
-    private JCheckBox abajoIzquierda;
-    private JCheckBox arribaCentro;
-    private JCheckBox centroCentro;
-    private JCheckBox abajoCentro;
-    private JCheckBox arribaDerecha;
-    private JCheckBox centroDerecha;
-    private JCheckBox abajoDerecha;
+    private JButton arribaIzquierda;
+    private JButton centroIzquierda;
+    private JButton abajoIzquierda;
+    private JButton arribaCentro;
+    private JButton centroCentro;
+    private JButton abajoCentro;
+    private JButton arribaDerecha;
+    private JButton centroDerecha;
+    private JButton abajoDerecha;
     private JButton chutar;
     private JButton parar;
 
 
     public JuegoMultiplayer() {
         // Configuramos el JFrame
-        setSize(1280,720);
+        setSize(1280, 720);
         setTitle("Penalty Eleven");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
@@ -44,40 +44,6 @@ public class JuegoMultiplayer extends JFrame {
         // Creamos los jugadores
         jugador1 = new Jugador1();
         jugador2 = new Jugador2();
-
-        // Creamos los checkbox
-        arribaIzquierda = new JCheckBox();
-        arribaDerecha = new JCheckBox();
-        arribaCentro = new JCheckBox();
-        centroIzquierda = new JCheckBox();
-        centroDerecha = new JCheckBox();
-        centroCentro = new JCheckBox();
-        abajoIzquierda = new JCheckBox();
-        abajoDerecha = new JCheckBox();
-        abajoCentro = new JCheckBox();
-
-        // Configuramos la posición de los checkbox
-        arribaIzquierda.setBounds(100, 100, 150, 150);
-        arribaDerecha.setBounds(1000, 100, 150, 150);
-        arribaCentro.setBounds(500, 100, 150, 150);
-        centroIzquierda.setBounds(100, 300, 150, 150);
-        centroDerecha.setBounds(1000, 300, 150, 150);
-        centroCentro.setBounds(500, 300, 150, 150);
-        abajoIzquierda.setBounds(100, 500, 150, 150);
-        abajoDerecha.setBounds(1000, 500, 150, 150);
-        abajoCentro.setBounds(500, 500, 150, 150);
-
-        // Configuramos el color de los checkbox
-        arribaIzquierda.setBackground(colorBaseBotones);
-        arribaDerecha.setBackground(colorBaseBotones);
-        arribaCentro.setBackground(colorBaseBotones);
-        centroIzquierda.setBackground(colorBaseBotones);
-        centroDerecha.setBackground(colorBaseBotones);
-        centroCentro.setBackground(colorBaseBotones);
-        abajoIzquierda.setBackground(colorBaseBotones);
-        abajoDerecha.setBackground(colorBaseBotones);
-        abajoCentro.setBackground(colorBaseBotones);
-
 
         // Añadimos los checkbox al panel
         panel.add(arribaIzquierda);
