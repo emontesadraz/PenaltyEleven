f6f337ee84062c6f6fa6aad1ec549ba507ac7ddf
PedroPineiro <ppineiroordax@danielcastelao.org>
2024-05-11 16:50:47 +0200

    optimizacion InterfazMaestra crear botones

--- src/InterfazMaestra.java
+++ src/InterfazMaestra.java
@@ -1,11 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.net.URL;
 
 public class InterfazMaestra extends JFrame {
+    public static final MusicManager musicManager = new MusicManager();
 
-    //Metodo para crear el fondo de la ventana
-    public static void crearFondo(JButton fondo,String rutaFondo) {
+//Metodo para crear el fondo de la ventana
+    public static void crearFondo(JButton fondo, String rutaFondo) {
         // Crear un botón para el fondo
         fondo.setBounds(0, 0, 1280, 720);
         fondo.setOpaque(false);
@@ -15,11 +18,30 @@ public class InterfazMaestra extends JFrame {
         // Cargar la imagen de fondo y establecerla como icono del botón
         URL url = InterfazMaestra.class.getClassLoader().getResource(rutaFondo);
         ImageIcon icono = new ImageIcon(url);
-        Image imagen = icono.getImage().getScaledInstance(1280, 720, Image.SCALE_SMOOTH);
+        Image imagen = icono.getImage().getScaledInstance(1280, 720, Image.SCALE_DEFAULT);
         icono = new ImageIcon(imagen);
         fondo.setIcon(icono);
     }
 
-    //Metodo para poner el logo de la aplicacion
+//Metodo para crear un botón
+    public static void crearBoton(JButton boton, String texto, int posX, int posY, int ancho, int alto, Color colorFondoBotones, Color colorTextoBotones, Font fuenteBoton, String rutaSoundEfectHover, float volumenSoundEfectHover) {
+        boton.setText(texto);
+        boton.setBounds(posX, posY, ancho, alto);
+        boton.setBackground(colorFondoBotones);
+        boton.setForeground(colorTextoBotones);
+        boton.setFont(fuenteBoton);
 
+        // Cambiar color de los botones al pasar el ratón por encima
+        boton.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent e) {
+                boton.setBackground(colorFondoBotones.darker());
+                musicManager.playSound(rutaSoundEfectHover, volumenSoundEfectHover);
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                boton.setBackground(colorFondoBotones);
+            }
+        });
+
+    }
 }
--- src/MenuInicial.java
+++ src/MenuInicial.java
@@ -1,17 +1,15 @@
-import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Font;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.io.IOException;
-import java.net.URL;
 
 public class MenuInicial extends InterfazMaestra {
-    private final JButton soloPlayer, multiPlayer, ranking, creditos, salir,fondo;
+    public static JPanel panelMenuInicial;
     public static final Color colorBaseBotones = new Color(25, 25, 25);
+    public static final Font fuenteBoton = new Font("Action Man", Font.BOLD, 20);
+    public static final Color colorTexto = new Color(255, 255, 255);
     private final MusicManager musicManager = new MusicManager();
 
     public MenuInicial() {
@@ -22,35 +20,44 @@ public class MenuInicial extends InterfazMaestra {
         setLocationRelativeTo(null);
         setResizable(false);
 
-        //Poner icono de la aplicación
+        //Crear panel
+        panelMenuInicial = new JPanel();
+        panelMenuInicial.setOpaque(false);
+        panelMenuInicial.setLayout(null);
+
+//Poner icono de la aplicación
         setIconImage(new ImageIcon("src/Imagenes/Logo.png").getImage());
 
-        //Crear panel
-        JPanel panel = new JPanel();
-        panel.setOpaque(false);
-        panel.setLayout(null);
+//Inicializar botones
+        JButton soloPlayer = new JButton();
+        JButton multiPlayer = new JButton();
+        JButton ranking = new JButton();
+        JButton creditos = new JButton();
+        JButton salir = new JButton();
 
-        //Crear botones
-        soloPlayer=new JButton("Un Jugador");
-        multiPlayer=new JButton("Multijugador");
-        ranking =new JButton("Ranking de Goleadores");
-        creditos=new JButton("Créditos");
-        salir=new JButton("Salir");
-
-        fondo = new JButton();
+// Fondo
+        JButton fondo = new JButton();
         crearFondo(fondo,"Imagenes/Fondo/FondoMenuInicial.png");
 
-        // Añadir los otros botones al panel
-        panel.add(soloPlayer);
-        panel.add(multiPlayer);
-        panel.add(ranking);
-        panel.add(creditos);
-        panel.add(salir);
-        panel.add(fondo);
+// Crear botones
+        crearBoton(soloPlayer, "Un Jugador", 120, 275, 460, 45, colorBaseBotones, colorTexto, fuenteBoton, "Musica/SoundEffect/SonidoSeleccion.wav", 0.6f);
+        crearBoton(multiPlayer, "Multijugador", 120, 355, 460, 45, colorBaseBotones, colorTexto, fuenteBoton, "Musica/SoundEffect/SonidoSeleccion.wav", 0.6f);
+        crearBoton(ranking, "Ranking de Goleadores", 120, 435, 460, 45, colorBaseBotones, colorTexto, fuenteBoton, "Musica/SoundEffect/SonidoSeleccion.wav", 0.6f);
+        crearBoton(creditos, "Créditos", 120, 515, 460, 45, colorBaseBotones, colorTexto, fuenteBoton, "Musica/SoundEffect/SonidoSeleccion.wav", 0.6f);
+        crearBoton(salir, "Salir", 120, 595, 460, 45, colorBaseBotones, colorTexto, fuenteBoton, "Musica/SoundEffect/SonidoSeleccion.wav", 0.6f);
 
-        //Añadir panel al JFrame
-        add(panel);
+// Añadir los botones al panel
+        panelMenuInicial.add(soloPlayer);
+        panelMenuInicial.add(multiPlayer);
+        panelMenuInicial.add(ranking);
+        panelMenuInicial.add(creditos);
+        panelMenuInicial.add(salir);
+        panelMenuInicial.add(fondo);
 
+//Añadir panel al JFrame
+        add(panelMenuInicial);
+
+//Acciones de los botones
         //Accion del boton soloPlayer
         soloPlayer.addActionListener(new ActionListener() {
             @Override
@@ -59,7 +66,7 @@ public class MenuInicial extends InterfazMaestra {
                 soloPlayer.setVisible(true);
                 dispose();
 
-                playSound("Musica/SoundEffect/SonidoElegir1.wav", 0.7f);
+                musicManager.playSound("Musica/SoundEffect/SonidoElegir1.wav", 0.7f);
                 musicManager.stopMusic();
             }
         });
@@ -76,7 +83,7 @@ public class MenuInicial extends InterfazMaestra {
                 }
                 selectorEquipos.setVisible(true);
                 dispose();
-                playSound("Musica/SoundEffect/SonidoElegir1.wav", 0.7f);
+                musicManager.playSound("Musica/SoundEffect/SonidoElegir1.wav", 0.7f);
                 musicManager.stopMusic();
             }
         });
@@ -88,7 +95,7 @@ public class MenuInicial extends InterfazMaestra {
                 Ranking ranking = new Ranking();
                 ranking.setVisible(true);
                 dispose();
-                playSound("Musica/SoundEffect/SonidoElegir1.wav", 0.7f);
+                musicManager.playSound("Musica/SoundEffect/SonidoElegir1.wav", 0.7f);
                 musicManager.stopMusic();
             }
         });
@@ -100,7 +107,7 @@ public class MenuInicial extends InterfazMaestra {
                 Creditos creditos = new Creditos();
                 creditos.setVisible(true);
                 dispose();
-                playSound("Musica/SoundEffect/SonidoElegir1.wav", 0.7f);
+                musicManager.playSound("Musica/SoundEffect/SonidoElegir1.wav", 0.7f);
                 musicManager.stopMusic();
             }
         });
@@ -109,128 +116,13 @@ public class MenuInicial extends InterfazMaestra {
         salir.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                playSound("Musica/SoundEffect/SonidoElegir1.wav", 0.7f);
+                musicManager.playSound("Musica/SoundEffect/SonidoElegir1.wav", 0.7f);
                 System.exit(0);
             }
         });
 
-        //Cambiar color base de los botones
-        soloPlayer.setBackground(colorBaseBotones);
-        multiPlayer.setBackground(colorBaseBotones);
-        ranking.setBackground(colorBaseBotones);
-        creditos.setBackground(colorBaseBotones);
-        salir.setBackground(colorBaseBotones);
-
-        //Cambiar tamaño y posición de los botones
-        soloPlayer.setBounds(120,275,460,45);
-        multiPlayer.setBounds(120,355,460,45);
-        ranking.setBounds(120,435,460,45);
-        creditos.setBounds(120,515,460,45);
-        salir.setBounds(120,595,460,45);
-
-        //Cambiar la fuente de los botones
-        Font fuenteBoton = new Font("Action Man", Font.BOLD, 20);
-        soloPlayer.setFont(fuenteBoton);
-        multiPlayer.setFont(fuenteBoton);
-        ranking.setFont(fuenteBoton);
-        creditos.setFont(fuenteBoton);
-        salir.setFont(fuenteBoton);
-
-        //Cambiar color de texto de los botones
-        Color colorTexto = new Color(255, 255, 255);
-        soloPlayer.setForeground(colorTexto);
-        multiPlayer.setForeground(colorTexto);
-        ranking.setForeground(colorTexto);
-        creditos.setForeground(colorTexto);
-        salir.setForeground(colorTexto);
-
-        // Cambiar color de los botones al pasar el ratón por encima
-        soloPlayer.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                playSound("Musica/SoundEffect/SonidoSeleccion.wav", 0.7f);
-                soloPlayer.setBackground(colorBaseBotones.darker());
-            }
-            @Override
-            public void mouseExited(MouseEvent e) {
-                soloPlayer.setBackground(colorBaseBotones);
-            }
-        });
-
-        multiPlayer.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                playSound("Musica/SoundEffect/SonidoSeleccion.wav", 0.7f);
-                multiPlayer.setBackground(colorBaseBotones.darker());}
-            @Override
-            public void mouseExited(MouseEvent e) {
-                multiPlayer.setBackground(colorBaseBotones);
-            }
-        });
-
-        ranking.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                playSound("Musica/SoundEffect/SonidoSeleccion.wav", 0.7f);
-                ranking.setBackground(colorBaseBotones.darker());}
-            @Override
-            public void mouseExited(MouseEvent e) {
-                ranking.setBackground(colorBaseBotones);
-            }
-        });
-
-        creditos.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                playSound("Musica/SoundEffect/SonidoSeleccion.wav", 0.7f);
-                creditos.setBackground(colorBaseBotones.darker());}
-            @Override
-            public void mouseExited(MouseEvent e) {
-                creditos.setBackground(colorBaseBotones);
-            }
-        });
-
-        salir.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                playSound("Musica/SoundEffect/SonidoSeleccion.wav", 0.7f);
-                salir.setBackground(colorBaseBotones.darker());}
-            @Override
-            public void mouseExited(MouseEvent e) {
-                salir.setBackground(colorBaseBotones);
-            }
-        });
-
-        // Controles de la música
-        musicManager.playMusic("Musica/Soundtrack/MenuInicial.wav", 0.2f);
+// Controles de la música
+        musicManager.playMusic("Musica/Soundtrack/MenuInicial.wav", 0.4f);
 
     }
-
-    public void playSound(String soundFile, float volume) {
-        try {
-            // Abrir un audio input stream
-            URL url = this.getClass().getClassLoader().getResource(soundFile);
-            AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
-
-            // Obtener un clip de sonido
-            Clip clip = AudioSystem.getClip();
-
-            // Abrir el clip de audio y cargar muestras de audio del audio input stream
-            clip.open(audioIn);
-
-            // Obtener el control de volumen
-            FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-
-            // Convertir el volumen en decibelios
-            float dB = (float) (Math.log(volume) / Math.log(10.0) * 20.0);
-            volumeControl.setValue(dB);
-
-            // Iniciar la reproducción
-            clip.start();
-        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
-            e.printStackTrace();
-        }
-
-    }
-
 }
