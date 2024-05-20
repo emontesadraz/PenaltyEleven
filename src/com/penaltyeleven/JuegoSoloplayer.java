6f058f9c59db369a3a8c9635c869dc715e7118c9
PedroPineiro <ppineiroordax@danielcastelao.org>
2024-05-20 11:50:35 +0200

    juego actualizacion

--- src/com/penaltyeleven/JuegoSoloplayer.java
+++ src/com/penaltyeleven/JuegoSoloplayer.java
@@ -5,7 +5,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class JuegoMultiplayer extends InterfazMaestra {
+public class JuegoSoloplayer extends InterfazMaestra {
+    private final MusicManager musicManager = new MusicManager();
     private static final int NUM_PENALES = 5;
     private int turno = 0;
     private int penalesRestantes1 = NUM_PENALES;
@@ -18,18 +19,12 @@ public class JuegoMultiplayer extends InterfazMaestra {
     private int[] porteroSeleccion = new int[4];
     private int seleccionPorteroCount = 0;
 
-    public static final Color colorBaseBotones = new Color(25, 25, 25);
-    public static final Font fuenteBoton = new Font("Rubik", Font.PLAIN, 20);
-    public static final Color colorTexto = new Color(255, 255, 255);
-    private final MusicManager musicManager = new MusicManager();
-
-    private JButton accionBoton = new JButton();
-    private JButton seguirBoton = new JButton("Seguir");
-    private final JButton[][] botones = new JButton[3][3];
+    private JButton[][] botones = new JButton[3][3];
+    private JButton accionBoton = new JButton("Tirar");
     private JLabel marcadorLabel = new JLabel("Jugador 1: 0 | Jugador 2: 0");
     private JLabel estadoLabel = new JLabel("Jugador 1 tira");
 
-    public JuegoMultiplayer() {
+    public JuegoSoloplayer() {
         setTitle("Penalty Eleven");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1280, 720);
@@ -39,26 +34,15 @@ public class JuegoMultiplayer extends InterfazMaestra {
         setIconImage(new ImageIcon("src/Imagenes/Logo.png").getImage());
         // Controles de la música
 
-        JPanel porteriaPanel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                // Carga la imagen de fondo
-                ImageIcon imagenFondo = new ImageIcon("src/Imagenes/Fondo/porteria3.png");
-                // Dibuja la imagen de fondo
-                g.drawImage(imagenFondo.getImage(), 0, 0, getWidth(), getHeight(), null);
-            }
-        };
-        porteriaPanel.setLayout(null); // Usamos un layout null para poder posicionar los elementos manualmente
+        JPanel porteriaPanel = new JPanel();
+        porteriaPanel.setLayout(new GridLayout(3, 3));
 
-        // Ahora añadimos los botones a este panel
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 final int x = i;
                 final int y = j;
                 botones[i][j] = new JButton();
-                botones[i][j].setContentAreaFilled(false);
-                botones[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+                botones[i][j].setBackground(Color.WHITE);
                 botones[i][j].addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent e) {
                         if (!seleccionPortero) {
@@ -70,37 +54,13 @@ public class JuegoMultiplayer extends InterfazMaestra {
                         }
                     }
                 });
-                // Establecemos las coordenadas y el tamaño de cada botón
-                int anchoBoton = 427; // ajusta este valor según tus necesidades
-                int altoBoton = 200; // ajusta este valor según tus necesidades
-                botones[i][j].setBounds(j * anchoBoton, i * altoBoton, anchoBoton, altoBoton);
                 porteriaPanel.add(botones[i][j]);
             }
         }
 
-        // Boton Tirar/Parar
-        crearBoton(accionBoton, "Tirar", 120, 595, 460, 45, colorBaseBotones, colorTexto, fuenteBoton, "Musica/SoundEffect/SonidoSeleccion.wav", 0.6f);
-
-        // Boton Seguir
-        crearBoton(seguirBoton, "Seguir", 700, 595, 460, 45, colorBaseBotones, colorTexto, fuenteBoton, "Musica/SoundEffect/SonidoSeleccion.wav", 0.6f);
-        seguirBoton.setVisible(false);
-        seguirBoton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                musicManager.playSound("Musica/SoundEffect/SonidoElegir1.wav", 0.7f);
-                resetearBotones();
-                setBotonesContentAreaFilled(false);
-                seguirBoton.setVisible(false);
-                accionBoton.setEnabled(true);
-                estadoLabel.setText(jugador1Tira ? "Jugador 1 tira" : "Jugador 2 tira");
-                tiroActual[0]=0;
-                tiroActual[1]=0;
-            }
-        });
-
         JPanel controlPanel = new JPanel();
         accionBoton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                musicManager.playSound("Musica/SoundEffect/SonidoElegir1.wav", 0.7f);
                 if (jugador1Tira) {
                     if (!seleccionPortero) {
                         if (tiroActual[0] == 0 && tiroActual[1] == 0) {
@@ -120,7 +80,6 @@ public class JuegoMultiplayer extends InterfazMaestra {
                             jugador1Tira = false;
                             procesarTurno();
                             accionBoton.setText("Tirar");
-                            mostrarResultados();
                         }
                     }
                 } else {
@@ -142,7 +101,6 @@ public class JuegoMultiplayer extends InterfazMaestra {
                             jugador1Tira = true;
                             procesarTurno();
                             accionBoton.setText("Tirar");
-                            mostrarResultados();
                         }
                     }
                 }
@@ -150,7 +108,6 @@ public class JuegoMultiplayer extends InterfazMaestra {
         });
 
         controlPanel.add(accionBoton);
-        controlPanel.add(seguirBoton);
 
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
@@ -230,34 +187,8 @@ public class JuegoMultiplayer extends InterfazMaestra {
         }
     }
 
-    private void mostrarResultados() {
-        accionBoton.setEnabled(false);
-        botones[tiroActual[0]][tiroActual[1]].setBackground(Color.RED);
-        for (int i = 0; i < 2; i++) {
-            botones[porteroSeleccion[i * 2]][porteroSeleccion[i * 2 + 1]].setBackground(Color.GREEN);
-        }
-        setBotonesContentAreaFilled(true);
-        seguirBoton.setVisible(true);
-    }
-
-    private void setBotonesContentAreaFilled(boolean filled) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                botones[i][j].setContentAreaFilled(filled);
-            }
-        }
-    }
-
     private void actualizarMarcador() {
         marcadorLabel.setText("Jugador 1: " + aciertos1 + " | Jugador 2: " + aciertos2);
-    }
-
-    private void resetearBotones() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                botones[i][j].setBackground(Color.WHITE);
-            }
-        }
     }
 
     private void determinarGanador() {
@@ -274,6 +205,7 @@ public class JuegoMultiplayer extends InterfazMaestra {
     }
 
     public static void main(String[] args) {
-        new JuegoMultiplayer();
+        new JuegoSoloplayer();
     }
 }
+
